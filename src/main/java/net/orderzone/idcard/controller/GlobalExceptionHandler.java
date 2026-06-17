26638b4f8f8e5c09a3058e8ff96e6e09c931a6e9
Din Rasy <dinrasy@gmail.com>
2026-06-17 17:44:06 +0700

    Fix Whitelabel page issue by adding custom error.html and GlobalExceptionHandler

---
+++ src/main/java/net/orderzone/idcard/controller/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package net.orderzone.idcard.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception ex, Model model) {
+        logger.error("Unhandled exception occurred: ", ex);
+        model.addAttribute("message", ex.getMessage());
+        return "error";
+    }
+}
